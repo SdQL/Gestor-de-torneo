812d0d275dcc9148f673378c66f00fac684f258d
SDC <99742065+SdQL@users.noreply.github.com>
2025-05-09 23:19:19 -0500

    Refactor Equipo: inicializar goles y simplificar metodos.

--- src/Modelos/Equipo.java
+++ src/Modelos/Equipo.java
@@ -3,55 +3,39 @@ package Modelos;
 public class Equipo {
     private String nombre;
     private String pais;
-    private int golesFavor;
-    private int golesContra;
-    private int puntos;
+    private int golesFavor = 0;
+    private int golesContra = 0;
+    private int puntos = 0;
 
     public Equipo(String nombre, String pais) {
         this.nombre = nombre;
         this.pais = pais;
-        this.golesFavor = 0;
-        this.golesContra = 0;
-        this.puntos = 0;
     }
 
     public String getNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public String getPais() {
         return pais;
     }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
     public int getGolesFavor() {
         return golesFavor;
     }
-
-    public void setGolesFavor(int golesFavor) {
-        this.golesFavor = golesFavor;
-    }
-
     public int getGolesContra() {
         return golesContra;
     }
-
-    public void setGolesContra(int golesContra) {
-        this.golesContra = golesContra;
-    }
-
     public int getPuntos() {
         return puntos;
     }
 
-    public void setPuntos(int puntos) {
-        this.puntos = puntos;
+    public void agregarGoles(int golesFavor, int golesContra) {
+        this.golesFavor += golesFavor;
+        this.golesContra += golesContra;
     }
+
+    public void sumarPuntos(int puntos) {
+        this.puntos += puntos;
+    }
+
+
 }
