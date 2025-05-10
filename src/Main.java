94677ac10a08a26076723df02f128bfccc7642e9
SDC <99742065+SdQL@users.noreply.github.com>
2025-05-10 13:30:42 -0500

    Añadir funcionalidad para registrar equipos en el torneo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import Modelos.*;
+
+import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Torneo torneo = new Torneo();
+
+        registrarEquipos(torneo, sc);
+        torneo.imprimirTabla();
+        torneo.jugarTodosContraTodosIdaVuelta();
+    }
+
+    private static void registrarEquipos(Torneo torneo, Scanner sc){
+        for (int i = 0; i < 10; i++){
+            System.out.println("Ingrese el nombre del equipo " + (i + 1) + ": ");
+            String nombre = sc.nextLine();
+            System.out.println("Ingrese el pais del equipo " + (i + 1) + ": ");
+            String pais = sc.nextLine();
+
+            Equipo equipo = new Equipo(nombre, pais);
+            torneo.registrarEquipo(equipo);
+
+            System.out.println("Equipo registrado: " + nombre + " de " + pais);
+        }
     }
 }
