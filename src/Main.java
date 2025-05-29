4b2ef281574e59b29e869250b76eda71a5bc8070
SDC <99742065+SdQL@users.noreply.github.com>
2025-05-29 18:47:27 -0500

    feat: añadir solicitud de país para cada equipo al ingresar datos

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("Ingrese el nombre del equipo " + (i + 1) + ": ");
             String nombre = sc.nextLine();
+            System.out.println("Ingrese el pais del equipo " + (i + 1) + ": ");
             String pais = sc.nextLine();
 
             Equipo equipo = new Equipo(nombre, pais);
