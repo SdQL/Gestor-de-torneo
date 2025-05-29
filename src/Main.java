f4fd33919dafd78ff7cc7b8af85488a0b3ac5001
SdQL <sadavid2007@gmail.com>
2025-05-28 20:39:57 -0500

    Añadir logica para armar los grupos de octavos

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,20 @@ public class Main {
         registrarEquipos(torneo, sc);
         torneo.imprimirTabla();
         torneo.jugarTodosContraTodosIdaVuelta();
+
+        Equipo []clasificados = torneo.equiposClasificados();
+        Octavos octavos = new Octavos(clasificados);
+        octavos.mostrarGrupos();
+
+
     }
 
     private static void registrarEquipos(Torneo torneo, Scanner sc){
         for (int i = 0; i < 10; i++){
             System.out.println("Ingrese el nombre del equipo " + (i + 1) + ": ");
             String nombre = sc.nextLine();
-            System.out.println("Ingrese el pais del equipo " + (i + 1) + ": ");
-            String pais = sc.nextLine();
+            //System.out.println("Ingrese el pais del equipo " + (i + 1) + ": ");
+            String pais = "Colombia";
 
             Equipo equipo = new Equipo(nombre, pais);
             torneo.registrarEquipo(equipo);
---
+++ src/Modelos/Octavos.java
@@ -0,0 +1,40 @@
+package Modelos;
+
+public class Octavos {
+
+    private Equipo[] clasificados;
+    private Equipo[] grupoA;
+    private Equipo[] grupoB;
+
+    public Octavos(Equipo[] clasificados) {
+        this.clasificados = clasificados;
+        this.grupoA = new Equipo[4];
+        this.grupoB = new Equipo[4];
+        armarGrupos();
+    }
+
+    public void mostrarGrupos() {
+        System.out.println("Grupo A:");
+        for (Equipo e : grupoA) {
+            System.out.println("- " + e.getNombre());
+        }
+
+        System.out.println("Grupo B:");
+        for (Equipo e : grupoB) {
+            System.out.println("- " + e.getNombre());
+        }
+    }
+
+    private void armarGrupos(){
+        int indexA=0, indexB=0;
+        for (int i = 0; i < clasificados.length; i++){
+            if((i+1) % 2 == 0){
+                grupoA[indexA] = clasificados[i];
+                indexA++;
+            } else {
+                grupoB[indexB] = clasificados[i];
+                indexB++;
+            }
+        }
+    }
+}
